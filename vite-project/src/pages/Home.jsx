0b69bd0e089cd4d8a50f6821e84cc486124c0bd7
MichaelX <michaelxu644@gmail.com>
2025-08-12 19:15:57 -0700

    Update site title and Home.jsx Content

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Home = () => {
     return (
@@ -27,6 +28,8 @@ export const Home = () => {
         </main>
         {/* Footer */}
         <Footer />
+
+        <Analytics />
       </div>
     );
 };

3d72252f5eb062859dbb7e8ad36249159994b3af
MichaelX <michaelxu644@gmail.com>
2025-08-12 22:00:50 -0700

    feat: added button/link to my github

--- vite-project/src/components/ProjectsSection.jsx
+++ vite-project/src/components/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import { ExternalLink } from "lucide-react"
+import { ArrowRight, ExternalLink } from "lucide-react"
 
 const projects = [
     {
@@ -73,6 +73,16 @@ export const ProjectsSection = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a 
+                className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                target="_blank"
+                href="https://github.com/MichaelShoo"
+                >
+                    Check my Github <ArrowRight size={16}/>
+                </a>
             </div>
         </div>
     </section>
